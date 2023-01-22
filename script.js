79a9fbd8f820ce30ee5e00c6fcd9a4927bd179e2
Yasin <kurucuu@hotmail.com>
2023-01-22 16:22:51 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function creatHeart(){
+    const heart = document.createElement('div');
+    heart.classList.add('heart');
+
+    heart.style.left = Math.random() * 100 + 'vw';
+    heart.style.animationDuration = Math.random() * 2 + 3 + 's';
+
+    heart.innerText = '🕷 ';
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove()
+    },5000)
+}
+
+setInterval(creatHeart, 300);
